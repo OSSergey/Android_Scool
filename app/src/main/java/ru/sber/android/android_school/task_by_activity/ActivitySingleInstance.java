29de744e52ae191abaa7e4d107ecba7dfb4a8277
Osinnikov S.S <s.s.osinnikov@gmail.com>
2021-06-29 00:15:06 +0300

    обавил часть задания из лекции 2

---
+++ app/src/main/java/ru/sber/android/android_school/task_by_activity/ActivitySingleInstance.java
@@ -0,0 +1,42 @@
+package ru.sber.android.android_school.task_by_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import ru.sber.android.android_school.R;
+
+public class ActivitySingleInstance extends AppCompatActivity {
+    private static int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buttons);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("ActivitySingleInstance number: " + String.valueOf(++count));
+        findViewById(R.id.button_standard).setOnClickListener(v -> startStandard());
+        findViewById(R.id.button_single_task).setOnClickListener(v -> startSingleTask());
+        findViewById(R.id.button_single_top).setOnClickListener(v -> startSingleTop());
+        findViewById(R.id.button_single_instance).setOnClickListener(v -> startSingleInstance());
+    }
+
+    private void startStandard() {
+        startActivity(new Intent(this, ActivityStandard.class));
+    }
+
+    private void startSingleTask() {
+        startActivity(new Intent(this, ActivitySingleTask.class));
+    }
+
+    private void startSingleTop() {
+        startActivity(new Intent(this, ActivitySingleTop.class));
+    }
+
+    private void startSingleInstance() {
+        startActivity(new Intent(this, ActivitySingleInstance.class));
+    }
+}
